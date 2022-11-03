b0f8f14c72bb8afd3c690ea5cdfa9da649837f3d
FarrellPHarimanto <117286217+FarrellPHarimanto@users.noreply.github.com>
2022-11-03 08:56:24 +0700

    Add files via upload

---
+++ Xilinx_VHDL_Code/digitalfilter_TB.vhd
@@ -0,0 +1,110 @@
+--------------------------------------------------------------------------------
+-- Company: 
+-- Engineer:
+--
+-- Create Date:   21:55:38 01/25/2022
+-- Design Name:   
+-- Module Name:   D:/BINUS Work/Lab Digital System/Final project/IIR/digitalfilter_TB.vhd
+-- Project Name:  IIR
+-- Target Device:  
+-- Tool versions:  
+-- Description:   
+-- 
+-- VHDL Test Bench Created by ISE for module: digitalfilteriir
+-- 
+-- Dependencies:
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+--
+-- Notes: 
+-- This testbench has been automatically generated using types std_logic and
+-- std_logic_vector for the ports of the unit under test.  Xilinx recommends
+-- that these types always be used for the top-level I/O of a design in order
+-- to guarantee that the testbench will bind correctly to the post-implementation 
+-- simulation model.
+--------------------------------------------------------------------------------
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+use IEEE.NUMERIC_STD.all;
+use IEEE.STD_LOGIC_UNSIGNED.all;
+ 
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--USE ieee.numeric_std.ALL;
+ 
+ENTITY digitalfilter_TB IS
+END digitalfilter_TB;
+ 
+ARCHITECTURE behavior OF digitalfilter_TB IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT digitalfilteriir
+    PORT(
+         clk : IN  std_logic;
+         shift_en : IN  std_logic;
+         input : IN  std_logic_vector(7 downto 0);
+         output : OUT  std_logic_vector(7 downto 0);
+         RST : IN  std_logic
+        );
+    END COMPONENT;
+    
+
+   --Inputs
+   signal clk : std_logic := '0';
+   signal shift_en : std_logic := '0';
+   signal input : std_logic_vector(7 downto 0) := (others => '0');
+   signal RST : std_logic := '0';
+
+ 	--Outputs
+   signal output : std_logic_vector(7 downto 0);
+
+   -- Clock period definitions
+   constant clk_period : time := 10 ns;
+ 
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: digitalfilteriir PORT MAP (
+          clk => clk,
+          shift_en => shift_en,
+          input => input,
+          output => output,
+          RST => RST
+        );
+
+-- Clock process definitions
+   clk_process :process
+   begin
+		clk <= '0';
+		wait for clk_period/2;
+		clk <= '1';
+		wait for clk_period/2;
+   end process;
+ 
+	
+--reset
+	rst_proc: process
+	begin
+		rst <= '1';
+		wait for 10 ns;
+		rst <= '0';
+		wait;
+	end process;
+	
+--stim
+   in_proc: process
+   begin        
+		for i in 1 to 100 loop 
+		 shift_en <= '1';
+		 input <= input + "00101011";
+		 wait for 10 ns;
+		 shift_en <= '0';
+		 wait for 4000 ns;
+		end loop;
+      wait;
+   end process;
+	
+END;
---
+++ Xilinx_VHDL_Code/top_module.vhd
@@ -0,0 +1,96 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    23:25:41 02/01/2022 
+-- Design Name: 
+-- Module Name:    top_module - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+use IEEE.STD_LOGIC_UNSIGNED.all;
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Top_module is
+	Port ( clk : in std_logic;
+			 rst : in std_logic;
+			 output : out std_logic_vector(15 downto 0);
+			 i_RX_Serial :in std_logic
+			 );
+end Top_module;
+
+architecture Behavioral of Top_module is
+
+component digitalfilteriir is
+    generic(
+  x : INTEGER := 16; -- size of multiplicand (M)
+  y : INTEGER := 16  -- size of multiplier (Q)
+ );
+    Port (  
+	 clk: in std_logic; -- i_clk
+	 rst : in std_logic;
+    shift_en: in std_logic; -- r_RX_DV
+    input: in std_logic_vector(x-1 downto 0); -- o_rx_byte
+    output: out std_logic_vector(y-1 downto 0)
+	 );
+    
+end component;
+
+component recieverIIR is
+  generic (
+    g_CLKS_PER_BIT : integer := 834     -- Needs to be set correctly
+    );
+  port (
+    i_Clk       : in  std_logic;
+    i_RX_Serial : in  std_logic;
+    o_RX_DV     : out std_logic;
+    o_RX_Byte   : out std_logic_vector(7 downto 0):="00000000";
+	 bit_16_RX_Byte : out std_logic_vector(15 downto 0):="0000000000000000"
+    );
+end component;
+
+--signal shift_en : std_logic;
+signal o_RX_DV : std_logic;
+signal shift : std_logic;
+signal input : std_logic_vector (15 downto 0);
+signal o_RX_byte: std_logic_vector (7 downto 0);
+--signal data : std_logic_vector(15 downto 0);
+
+begin
+
+tx_digitalfilteriir : digitalfilteriir
+	Port map (
+				clk => clk,
+				rst => rst,
+            shift_en => shift,
+            input => input,
+            output => output
+	);
+tx_recieverIIR : recieverIIR
+	Port map (
+				i_Clk   => clk,
+				i_RX_Serial => i_RX_Serial, 
+				o_RX_DV => shift,   
+				o_RX_Byte => o_RX_Byte, 
+				bit_16_RX_Byte => input
+	);
+
+end Behavioral;
